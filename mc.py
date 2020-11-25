76fda2e310d0b82cdc8ddf05d86d98d7be00663e
RevonZev <revonzev@protonmail.com>
2020-11-25 20:14:20 +0700

    0.2.0 Export to File

--- mc.py
+++ mc.py
@@ -28,43 +28,44 @@ def tabsReader(lines:list):
 
 
 def syntaxMatcher(lines:list):
+	# TODO: REFACTOR
 	regex = {"pattern": r"^as\sat\s.+:$", "command": "as {value} at @s", "replace": ["as at ", ":"], "execute": True}
 	converter_settings = json.loads(rw.read("./converter.json"))
 	# parent = {"tabs": 0, "value": "", "execute": False}
 	parent = {"execute": False, "tabs": 0, "tabs_value": [""]}
 	new_lines = []
 	command = ""
-	execute_part = False
+	execute_part = [False]
 	for line in lines:
 		# Non-Execute
 		for execute in converter_settings["execute"]:
 			regex2 = r"{}".format(execute["pattern"])
 			if re.match(regex2, line["value"]) != None:
-				execute_part = True
+				execute_part = [True, execute]
+				break
 			else:
-				execute_part = False
-
-		if re.match(regex["pattern"], line["value"]) != None:
-			for i in regex["replace"]:
+				execute_part = [False, execute]
+		if line["tabs"] <= parent["tabs"]:
+			parent["execute"] = False
+			parent["tabs_value"] = [""]
+		
+		if execute_part[0]:
+			for i in execute_part[1]["replace"]:
 				if i == ":":
 					line["value"] = line["value"][:-1:]
 					continue
 				line["value"] = line["value"].replace(i, "")
-			command = regex["command"].replace("{value}", line["value"])
+			command = execute_part[1]["command"].replace("{value}", line["value"])
 			parent["execute"] = True
-		# Execute
 			parent["tabs_value"].append(parent["tabs_value"][line["tabs"]] + " " + command)
+			print(parent)
 		elif re.match(r"(//|#) .+", line["value"]) != None:
 			continue
 		else:
-			if line["tabs"] <= parent["tabs"]:
-					parent["execute"] = False
-					parent["tabs_value"] = [""]
-
 			if parent["execute"] == True:
 				command = parent["tabs_value"][line["tabs"]] + " run " + line["value"]
 				if re.match(r"execute", command) == None:
-					command = "execute " + command
+					command = "execute" + command
 			else:
 				command = line["value"]
 
